b8e95d443d47174b973ca5a454b77bb6ee0f18b8
lUCI <lusindisontanjana@gmail.com>
2022-11-10 10:09:14 +0200

    Add reducersand actions

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categories = createSlice({
+	name: 'category',
+	initialState: { category: '' },
+	reducers: {
+		checkStatus(state) {
+			// eslint-disable-next-line no-param-reassign
+			state.category = 'Under construction';
+		},
+	},
+});
+export const categoriesActions = categories.actions;
+
+export default categories;
