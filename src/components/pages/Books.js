3e39e4f8d5ed6118b670ec995b33288043a1bdbe
lUCI <lusindisontanjana@gmail.com>
2022-11-10 10:54:05 +0200

    Add addbook functionality

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,15 +1,33 @@
-import Book from '../Book';
-import AddBook from '../UI/AddBook';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { booksActions } from '../../redux/books/books';
+import classes from './Book.module.css';
 
-const Books = () => (
-  <main>
-    <div className="container">
-      <Book author="Suzanne Collins" title="The Hunger Games" />
-      <Book author="Frank Herbit" title="Dune" />
-      <Book author="Suzanne Collins" title="Capital in the Twenty-First Century" />
-      <AddBook />
+const Book = ({ id, title, author }) => {
+  const dispatch = useDispatch();
+
+  const removeBookHandler = (e) => {
+    dispatch(booksActions.removeBook(e.target.id));
+  };
+  return (
+    <div className={classes.book}>
+      <h3 className="title">{title}</h3>
+      <div className="author">{author}</div>
+      <div className={classes.buttons}>
+        <button type="button">Comments</button>
+        <button id={id} type="button" onClick={removeBookHandler}>
+          Remove
+        </button>
+        <button type="button">Edit</button>
+      </div>
     </div>
-  </main>
-);
+  );
+};
 
-export default Books;
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default Book;
