47deeca6b0b46933302cd9ea0ccf1b529b71dfb1
Lusindiso Ntanjana <lusindisontanjana@gmail.com>
2023-01-18 12:56:55 +0200

    Initial REST API for books

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,33 +1,54 @@
+const Book = require("../models/bookModel");
 const asyncHanlder = require("express-async-handler");
 
 // @desc    Get books
 // @route   Get api/books
 // @access  Private
 exports.getBooks = asyncHanlder(async (req, res) => {
-  res.status(200).json({ message: "Get books" });
+  const books = await Book.find();
+  res.status(200).json(books);
 });
 
 // @desc    Set book
 // @route   POST api/books
 // @access  Public
 exports.setBooks = asyncHanlder(async (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error("Please add a text field");
-  }
-  res.status(201).json({ message: "Set books" });
+  const { title, author, category } = req.body;
+
+  const book = await Book.create({
+    title,
+    author,
+    category,
+  });
+
+  res.status(201).json(book);
 });
 
 // @desc    Update book
 // @route   PUT api/books/:id
 // @access  Private
 exports.updateBooks = asyncHanlder(async (req, res) => {
-  res.status(200).json({ message: `Update book ${req.params.id}` });
+  const book = await Book.findById(req.params.id);
+  if (!book) {
+    res.status(400);
+    throw new Error("Book not found!");
+  }
+
+  const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updateBook);
 });
 
 // @desc    Delete book
 // @route   DELETE api/books/:id
 // @access  Private
 exports.deleteBooks = asyncHanlder(async (req, res) => {
-  res.status(200).json({ message: `Delete book ${req.params.id}` });
+  const book = await Book.findById(req.params.id);
+  if (!book) {
+    res.status(400);
+    throw new Error("Book not found!");
+  }
+  await book.remove();
+  res.status(200).json({ id: req.params.id });
 });
---
+++ backend/models/bookModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please enter the book title"],
+    },
+    author: {
+      type: String,
+      required: [true, "Please enter the auth's name"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please select category"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
